f671c63e7aac79576777c0567ade0c6263d9d0dd
Justin Venegas <justin.venegas@gmail.com>
2016-02-20 15:16:27 -0700

    Adding rxtx tutorial sketch for arduino

---
+++ arduino/pclink/tutorial_sketch.ino
@@ -0,0 +1,10 @@
+// the setup function runs once when you press reset or power the board
+void setup() {
+  Serial.begin(9600);
+}
+
+// the loop function runs over and over again forever
+void loop() {
+  Serial.println("Hello World");
+  delay(1000);              // wait for a second
+}
